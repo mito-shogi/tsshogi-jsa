44501b2e097f82033add7f04eb22efadd9f663b6
tkgstrator <tkgstrator@users.noreply.github.com>
2025-07-21 15:31:17 +0000

    fix(models): handle out-of-range index in decodeGameList function

--- src/models/game.dto.ts
+++ src/models/game.dto.ts
@@ -13,6 +13,11 @@ const decodeGameList = (buffer: Buffer) => {
   const games: Buffer[] = []
   // biome-ignore lint/suspicious/noAssignInExpressions: reason
   while ((index = buffer.indexOf(Buffer.from([0x4b, 0x49]), index + 1)) !== -1) {
+    // インデックスが範囲外になる場合には終了する
+    if (index + 6 >= buffer.length) {
+      break
+    }
+    // 長さを取得する
     const length: number = buffer.readUInt32BE(index + 2)
     if (buffer.slice(index, index + length + 6).length > 0x28) {
       games.push(buffer.slice(index, index + length + 6))
