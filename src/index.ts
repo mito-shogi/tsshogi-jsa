f47216677e5de801442fdbdfcffb7de4007b2dd6
tkgstrator <nasawake.am@gmail.com>
2025-09-15 14:49:42 +0000

    feat(tournament): update tournament names and add new entry to list

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { Tournament } from '@/constant/tournament'
 export { Game, GameList } from '@/models/game.dto'
 export { GameInfo, Player } from '@/models/message.dto'
 export { decodeGameList, decodeJSA, importJSA } from '@/utils/jsa'
