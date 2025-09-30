6a5ddffc699582506d4fa2a86c2bcb4ba524efce
tkgstrator <nasawake.am@gmail.com>
2025-09-30 21:53:10 +0000

    refactor(models): update types and schemas for game and JSA objects

--- src/utils/jsa.ts
+++ src/utils/jsa.ts
@@ -1,6 +1,40 @@
 import { importCSA, type Record } from 'tsshogi'
+import type { Tournament } from '@/constant/tournament'
 import { type GameList, GameListObjectSchema } from '@/models/game.dto'
-import { type GameInfo, JSAObjectSchema } from '@/models/message.dto'
+import { JSAObjectSchema } from '@/models/message.dto'
+
+/**
+ * 棋譜情報
+ */
+export type GameInfo = {
+  game_id: number
+  black: {
+    first_name?: string
+    last_name?: string
+    name: string // RecordMetadataKey.BLACK_NAME
+    rank?: string
+    time_limit?: number | undefined // RecordMetadataKey.BLACK_TIME_LIMIT
+  }
+  white: {
+    first_name?: string
+    last_name?: string
+    name: string // RecordMetadataKey.WHITE_NAME
+    rank?: string | undefined
+    time_limit?: number | undefined // RecordMetadataKey.WHITE_TIME_LIMIT
+  }
+  metadata: {
+    date: string // RecordMetadataKey.DATE
+    time_limit: number | undefined // RecordMetadataKey.TIME_LIMIT
+    start_time: string // RecordMetadataKey.START_DATETIME
+    end_time: string | null // RecordMetadataKey.END_DATETIME
+    title: string // RecordMetadataKey.TITLE
+    tournament: Tournament | null // RecordMetadataKey.TOURNAMENT
+    length: number // RecordMetadataKey.LENGTH
+    place?: string // RecordMetadataKey.PLACE
+    strategy?: string // RecordMetadataKey.STRATEGY
+  }
+  kif: string
+}
 
 export const decodeJSA = (buffer: Buffer): GameInfo => {
   const result = JSAObjectSchema.safeParse(buffer)
