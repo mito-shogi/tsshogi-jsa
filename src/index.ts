b51f8433f8e4b80bd3bca22fb9fdc3f4a96ad7d8
tkgstrator <tkgstrator@users.noreply.github.com>
2025-09-30 21:37:44 +0000

    chore(dependencies): update version and adjust exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
 export { Tournament, TournamentList } from '@/constant/tournament'
-export { Game, GameList } from '@/models/game.dto'
-export { GameInfo, JSAObjectSchema as GameInfoSchema, Player } from '@/models/message.dto'
-export { decodeGameList, decodeJSA, importJSA } from '@/utils/jsa'
